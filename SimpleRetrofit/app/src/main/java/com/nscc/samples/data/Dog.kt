013a03d3fb1d348b70f83fe14b881f1ed9bc63cf
“DanielPenny” <Daniel.Penny@smu.ca>
2024-04-09 23:42:58 -0300

    Deleted files

--- SimpleRetrofit/app/src/main/java/com/nscc/samples/data/Dog.kt
+++
@@ -1,9 +0,0 @@
-package com.nscc.samples.data
-
-
-import com.google.gson.annotations.Expose
-import com.google.gson.annotations.SerializedName
-
-data class Dog(@SerializedName("message")@Expose val url:String,
-               @SerializedName("status")@Expose val status:String
-)
